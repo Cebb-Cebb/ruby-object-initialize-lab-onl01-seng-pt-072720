04deea270711de8bb96336c8167fbb84fc9a9885
hersson ceballos <herssonceballos@gmail.com>
2020-08-12 03:04:58 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,28 @@
+class Dog  
+  def initialize(name ,breed="Mutt")
+    @name = name
+    @breed = breed
+  end
+end 
+
+# fido = Dog.new("Mutt")
+ 
+# fido.breed #=> "Collie"
+
+ 
+  
+  # it 'defaults the breed argument to "Mutt" in an instance variable @breed' do
+  #     fido = Dog.new("Fido")
+
+
+
+
+# class Point
+#   def initialize(x, y)
+#     @x = x
+#     @y = y
+#   end
+
+
+
+  
