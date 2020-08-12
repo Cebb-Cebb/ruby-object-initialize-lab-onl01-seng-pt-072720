4c6697733643f9175a2956946546dcad5c733fa4
hersson ceballos <herssonceballos@gmail.com>
2020-08-12 02:31:35 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,62 @@
+class Dog  
+  
+  def initialize(name)
+    @name = name 
+  end
+  
+  def name=(name)
+    @name
+  end 
+  
+  def name
+    @name
+  end 
+  
+end 
+
+# class Person
+ 
+# def initialize(name)
+#   @name = name 
+# end 
+  
+#   def name=(name)
+#     @name 
+#   end 
+  
+#   def name 
+#     @name
+#   end 
+  
+# end
+
+
+
+  
+  
+# end
+
+# class Dog
+#   def initialize(breed)
+#     @breed = breed
+#   end
+ 
+#   def breed=(breed)
+#     @breed = breed
+#   end
+ 
+#   def breed
+#     @breed
+#   end
+# end
+
+
+# describe 'Dog' do
+#   describe '#initialize' do
+#     it 'sets the name of the dog in an instance variable @name' do
+#       fido = Dog.new("Fido", "Pug")
+
+#       expect(fido.instance_variable_get(:@name)).to eq("Fido")
+#     end
+
+
