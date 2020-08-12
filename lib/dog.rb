5bb07e1428505016506f2777525b58d8598f641e
hersson ceballos <herssonceballos@gmail.com>
2020-08-12 02:38:58 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,12 +3,11 @@ class Dog
     @name = name
     @breed = breed
   end
-end 
 
 fido = Dog.new("Mutt")
- 
 fido.breed 
-  
+
+end   
   
   # it 'defaults the breed argument to "Mutt" in an instance variable @breed' do
   #     fido = Dog.new("Fido")
